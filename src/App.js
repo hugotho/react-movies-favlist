3e889c680e78b5a2f6f0c6e1960d815a23d06a58
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-12-27 12:22:41 -0300

    adds context to authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+import AuthContext from './context/AuthContext';
 import About from './pages/about';
 import Home from './pages/home';
 import Movies from './pages/movies';
@@ -7,22 +9,24 @@ import Login from './pages/login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <h1>Lista de Filmes Favoritos</h1>
-      <nav>
-        <Link to="/">Meus Favoritos</Link>
-        <Link to="/Buscar">Buscar Filmes</Link>
-        <Link to="/Sobre">Sobre</Link>
-        <span>Olá visitante!</span>
-        <Link to="/Entrar">Entrar</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Buscar" element={<Movies />} />
-        <Route path="/Sobre" element={<About />} />
-        <Route path="/Entrar" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider>
+      <BrowserRouter>
+        <h1>Lista de Filmes Favoritos</h1>
+        <nav>
+          <Link to="/">Meus Favoritos</Link>
+          <Link to="/Buscar">Buscar Filmes</Link>
+          <Link to="/Sobre">Sobre</Link>
+          <span>Olá visitante!</span>
+          <Link to="/Entrar">Entrar</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Buscar" element={<Movies />} />
+          <Route path="/Sobre" element={<About />} />
+          <Route path="/Entrar" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
