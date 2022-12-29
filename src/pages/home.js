f41c136e504ed63e132ca7824eea5ffb734ffbd1
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-12-29 13:05:51 -0300

    Adds home not logged message

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,30 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import FavoritesContext from "../context/FavoritesContext";
+
 export default function Home() {
-    return (
+  const [token] = useContext(AuthContext);
+  const [favorites] = useContext(FavoritesContext);
+
+  return (
+    <div>
+      <h2>Meus favoritos</h2>
+      {token !== '' && (
+        <>
+          {favorites.length > 0 && (favorites.map(fav =>
+            <div>{fav}</div>
+          ))}
+        </>
+      )}
+      {token === '' && (
         <div>
-            <h2>Meus favoritos</h2>
+          <p>
+            Para ter acesso a esta funcionalidade realize seu <Link to="/Entrar">login</Link>.<br />
+            Não possui credenciais? <Link to="/Cadastrar">faça seu cadastro</Link>.
+          </p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
