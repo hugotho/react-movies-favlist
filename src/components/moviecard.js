cb2b43cc88535f1b86bf8d1aa83f43a45af51bf0
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-12-29 01:24:15 -0300

    Fix posters styling

--- src/components/moviecard.js
+++ src/components/moviecard.js
@@ -1,14 +1,23 @@
+import React from 'react';
+import MovieSvg from '../emptyposter.svg';
+
 export default function MovieCard(props) {
   const movie = props.movie
+  
   return (
     <div className="movie flex-container">
       <div className="poster-container">
-        <img className="poster" src={movie.Poster} alt={"Poster de '" + movie.Title + "'"}></img>
+        {movie.Poster !== "N/A" && (
+          <img className="poster" src={movie.Poster} alt={"Poster de '" + movie.Title + "'"}></img>
+        )}
+        {movie.Poster === "N/A" && (
+          <img className="poster" src={MovieSvg} alt={"Poster não disponibilizado"}></img>
+        )}
       </div>
       <div className="movie-data">
         <div><em>{movie.Type === "movie" ? "Filme" : movie.Type === "series" ? "Série" : "Episódio"}</em></div>
         <div className="flex-container">
-          <strong style={{"padding-right": "4px"}}><em>Título:</em></strong>
+          <strong style={{ "paddingRight": "4px" }}><em>Título:</em></strong>
           <span>{movie.Title}</span>
         </div>
         <div><strong><em>Ano: </em></strong>{movie.Year}</div>
